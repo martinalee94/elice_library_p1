121f7cf7dcf1d0b77e5d656527d3d62784b20dd2
Martina Lee <martinalee0528@gmail.com>
2021-11-22 22:29:10 +0900

    the number of rent books is working properly

--- app/dashboard.py
+++ app/dashboard.py
@@ -14,6 +14,8 @@ def dashboard(id):
         #     flash(msg)
         #     return redirect('/home')
         #else:
+        
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
         rent_list = db.session.query(Rent, Books).filter(Rent.book_id == Books.id).filter(Rent.user_id == session.get('login')).filter(Rent.status == 1).all()
         
         #이렇게 조인을 하면, 첫번째는 Rent객체, 두번쨰는 Books 객체가 결과값으로 반환된다
@@ -22,8 +24,8 @@ def dashboard(id):
         if len(history) == 0: #대여 이력이 없을때
             return render_template('dashboard.html', cur_count = 0, total_count = 0 )
 
-        today = datetime.datetime.now().strftime("%Y-%m-%d")
-        cur_count = db.session.query(func.count(Rent.user_id)).filter(Rent.status == 1).first()[0]
+        #cur_list = db.session.query(func.count(Rent.user_id)).filter(Rent.user_id == session.get('login')).all()
+        cur_count = len(rent_list)
         total_count = db.session.query(func.count(Rent.user_id)).group_by(Rent.user_id).having(Rent.user_id == session.get('login')).first()[0]
         return render_template('dashboard.html', book_list = rent_list, cur_count = cur_count, total_count = total_count, history = history, today= today )
     elif request.method == 'POST':
